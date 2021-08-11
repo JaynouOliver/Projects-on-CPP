32991cd3af8df7b2caff88bfe9165c47bfbaa47d
JaynouOliver <jyb2084.usa@gmail.com>
2021-08-10 20:28:34 -0400

    forloop
    Beginning with for loop

---
+++ forloop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+   for( int a = 0 ; a <= 10 ; a++){
+       cout << " This is Life" <<  endl; //we can also print a here instead of "This is life"
+   }
+
+    return 0;
+}
+
+

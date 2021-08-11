b54bbd370e414bb7f1181685da1298efe8f7a519
JaynouOliver <jyb2084.usa@gmail.com>
2021-08-10 20:40:13 -0400

    Creating While loop
    Learning While Loop

---
+++ whileloop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+   int a =1;
+   while (a <= 10)
+   {
+       cout << "Me" <<endl; //we can also print 'a' instead of "Me"//
+       a++;
+   }
+   
+
+    return 0;
+}
+
+

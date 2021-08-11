b9a77af8f7d148c07d0a9c5086e8fd58bfb435a9
JaynouOliver <jyb2084.usa@gmail.com>
2021-08-10 20:44:29 -0400

    Do While Loop
    Learning Do While Loop

---
+++ do while loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    
+   int a =1;
+   do
+   {
+       cout << "readMe" << endl;
+       a++;
+   } while (a <= 20);
+   
+
+    return 0;
+}
+
+

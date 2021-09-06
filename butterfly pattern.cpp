ba286df47bee51e8f1e96391535a80c36922e329
JaynouOliver <jyb2084.usa@gmail.com>
2021-09-05 20:04:23 -0400

    Butterfly Pattern
    *                          *
    * *                     * *
    * * *                * * *
    * * * *           * * * *
    * * * * *      * * * * *
    * * * * * * * * * * * *
    * * * * * * * * * * * *
    * * * * *       * * * * *
    * * * *            * * * *
    * * *                 * * *
    * *                      * *
    *                           *

---
+++ butterfly pattern.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE        // This is written for beautification.
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif                      // This is not part of the code.
+
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        int m = 2*n - 2*i;
+        for (int j = 1; j <= m; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;       
+    }
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        int m = 2*n - 2*i;
+        for (int j = 1; j <= m; j++)
+        {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;       
+    }
+    return 0;
+}

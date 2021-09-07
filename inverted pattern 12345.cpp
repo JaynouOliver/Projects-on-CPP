40e8b758573df4aa754cdf0e359d7ce63541e559
JaynouOliver <jyb2084.usa@gmail.com>
2021-09-07 19:24:45 -0400

    Inverted Patern 12345
    1 2 3 4 5
    1 2 3 4
    1 2 3
    1 2
    1

---
+++ inverted pattern 12345.cpp
@@ -0,0 +1,25 @@
+// Inverted Pattern 1 2 3 4 5 //
+
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE        // This is written for beautification.
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif                      // This is not part of the code.
+    
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}

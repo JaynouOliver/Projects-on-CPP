da0d481a143dbfdec253d5b0bdd151f4d4d6aed0
JaynouOliver <jyb2084.usa@gmail.com>
2021-09-07 20:56:31 -0400

    Number Pattern
          1
         1 2
        1 2 3
       1 2 3 4
      1 2 3 4 5
     1 2 3 4 5 6
    1 2 3 4 5 6 7

---
+++ Number Pattern.cpp
@@ -0,0 +1,27 @@
+// Number Pattern
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE        // This is written for beautification.
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif                      // This is not part of the code.
+    
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}

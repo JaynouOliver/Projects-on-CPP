35d60fe013aa2ecd0f8e7379bf78ffa60f5b304b
JaynouOliver <jyb2084.usa@gmail.com>
2021-09-21 00:36:48 -0400

    Palindrome pattern
            1
          212
        32123
      4321234
    543212345

---
+++ palindrome series.cpp
@@ -0,0 +1,39 @@
+// Palindrome Pattern
+#include <iostream>
+using namespace std;
+int main() {
+
+#ifndef ONLINE_JUDGE        // This is written for beautification.
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif                      // This is not part of the code.
+    
+    int n;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int j;
+        for (j = 1; j <= n - i; j++)
+        {
+            cout << " ";
+        }
+
+        int k = i;
+        for (; j <= n; j++)
+        {
+            cout << k-- ;
+        }
+        
+        k = 2;
+        for (; j <= n + i - 1; j++)
+        {
+            cout << k++;
+        }
+        
+        cout << endl;
+    }
+    
+
+    return 0;
+}
